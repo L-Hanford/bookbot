a0d5b2b8f708f2812b1c7a271bb62942d96b4f1a
root <root@DESKTOP-OVUR763.localdomain>
2024-02-13 17:57:54 -0800

    functional character and word counter

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
-def get_book_text(path):
-    word_counter = 0
-    letter_counter = 0
-    character_dictionary = {
-        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0,
-        'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0,
-        's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0,
-        '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0
-    }
+import time
 
-    with open(path) as f:
-        for word in f.read().split():
-            word_counter += 1
-            for letter in word.lower():
-                if letter in character_dictionary:
-                    character_dictionary[letter] += 1
-        return character_dictionary
+def main():
+    #Variables - path and character_dictionary which will be used to count each character within the provided text
+    book_path = "books/frankenstein.txt"
+    character_dictionary = {
+            'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0,
+            'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0,
+            's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0,
+            '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0
+                            }
+
+    #Finds the total amount of words within the provided text
+    read_text = get_book_text(book_path)
+    #Generates the reader object which allows us to iterate through the provided text
+    word_counter = book_word_counter(read_text)
+    #Generates the character counter as compared to the dictionary provided above vs the text provided
+    character_counter = book_character_counter(read_text,character_dictionary)
+    print(character_counter)
+
+def get_book_text(path_to_text):
+    with open(path_to_text) as book:
+        return book.read()
+
+def book_word_counter(text):
+    words = text.split()
+    return len(words)
+
+def book_character_counter(text,character_dictionary):
+    for character in text.lower():
+        if character in character_dictionary:
+            character_dictionary[character] += 1
+    return character_dictionary
 
 main()
